5783786fa73d0de650b21a1da89a75922edc1322
hariria <hariria@usc.edu>
2024-03-06 18:10:24 -0800

    [nextra] Port over Learn About Aptos

---
+++ apps/nextra/pages/en/network/blockchain/_meta.ts
@@ -0,0 +1,3 @@
+export default {
+  "learn-about-aptos": "Learn about Aptos"
+}
---
+++ apps/nextra/pages/en/network/blockchain/learn-about-aptos/_meta.ts
@@ -0,0 +1,19 @@
+const learnAboutAptos = {
+  blockchain: 'Aptos Blockchain Deep Dive',
+  'validator-nodes': 'Validator Nodes Overview',
+  'fullnodes': 'Fullnodes Overview',
+  'node-networks-sync': 'Node Networks and Sync',
+  'move': 'Move - A Web3 Language and Runtime',
+  accounts: 'Accounts',
+  resources: 'Resources',
+  events: 'Events',
+  'txns-states': 'Transactions and States',
+  'gas-txn-fee': 'Gas and Storage Fees',
+  'base-gas': 'Computing Transaction Gas',
+  'blocks': 'Blocks',
+  'staking': 'Staking',
+  'delegated-staking': 'Delegated Staking',
+  governance: 'Governance'
+}
+
+export default learnAboutAptos;
