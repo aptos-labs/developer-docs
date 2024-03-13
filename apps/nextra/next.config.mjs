e843ae0b9a997b81e82f2cfb132a448b8e7c8b0d
Andrew Hariri <hariria@usc.edu>
2024-03-13 13:12:07 -0700

    [nextra] Transpile packages to avoid module not found issue

--- apps/nextra/next.config.mjs
+++ apps/nextra/next.config.mjs
@@ -48,6 +48,7 @@ export default withBundleAnalyzer(
         permanent: true
       }
     ],
-    reactStrictMode: true
+    reactStrictMode: true,
+    transpilePackages: ['@aptos-labs/aptos-nextra-config']
   })
 )
