60039eed28084aa773b3e0532cfb24b71242c0cf
Andrew Hariri <hariria@usc.edu>
2024-03-26 15:06:08 -0700

    [nextra] Add hotjar support

--- apps/nextra/pages/_document.tsx
+++ apps/nextra/pages/_document.tsx
@@ -1,19 +1,38 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
-import { SkipNavLink } from 'nextra-theme-docs'
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
+import { SkipNavLink } from "nextra-theme-docs";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <Script
+            dangerouslySetInnerHTML={{
+              __html: `
+            <!-- Hotjar Tracking Code for my-hotjar-app -->
+            <script>
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:1234567,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            </script>
+          `,
+            }}
+          />
+        </Head>
         <body>
           <SkipNavLink styled />
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
