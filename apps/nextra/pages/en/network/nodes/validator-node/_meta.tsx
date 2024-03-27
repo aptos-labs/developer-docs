eff75e581d8478c59b2e5d6dc2da228c949aab5d
hariria <hariria@usc.edu>
2024-03-27 15:30:01 -0700

    [nextra] reorganize run a validator and vfn sections

---
+++ apps/nextra/pages/en/network/nodes/validator-node/_meta.tsx
@@ -0,0 +1,7 @@
+export default {
+  "node-requirements": "Node Requirements",
+  "deploy-nodes": "Deploy Nodes",
+  "connect-nodes": "Connect Nodes",
+  "verify-nodes": "Verify Nodes",
+  "modify-nodes": "Modify Nodes",
+}
