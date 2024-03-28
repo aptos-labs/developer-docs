47c7c1e9656cdc25b063620fe02c02f4eb860dc0
Andrew Hariri <hariria@usc.edu>
2024-03-28 12:56:37 -0700

    [nextra] Develop with local testnet, monitor a node reorganization

---
+++ apps/nextra/pages/en/network/nodes/_meta.tsx
@@ -0,0 +1,13 @@
+export default {
+  "local-testnet": "Develop with Local Testnet",
+  "validator-node": "Run a Validator and VFN",
+  "full-node": "Run a Public Fullnode",
+  "bootstrap-fullnode": "Bootstrap a Node",
+  "configure": "Configure a Node",
+  "measure": "Monitor a Node",
+  "networks": {
+    title: "Networks",
+    toc: false,
+    typesetting: 'article'
+  }
+}
---
+++ apps/nextra/pages/en/network/nodes/full-node/_meta.tsx
@@ -0,0 +1,6 @@
+export default {
+  "pfn-requirements": "PFN Requirements",
+  "deployments": "Deploy a PFN",
+  "verify-pfn": "Verify a PFN",
+  "modify-pfn": "Modify a PFN",
+}
