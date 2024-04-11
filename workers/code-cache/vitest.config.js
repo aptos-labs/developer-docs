9e0eb657c09033b38384a6262a82eae476318fee
hariria <hariria@usc.edu>
2024-04-11 00:11:26 -0700

    [code-cache] Setup vitest unit testing

---
+++ workers/code-cache/vitest.config.js
@@ -0,0 +1,14 @@
+import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
+
+export default defineWorkersConfig({
+  test: {
+    poolOptions: {
+      workers: {
+        wrangler: { configPath: "./wrangler.toml" },
+        miniflare: {
+          d1Databases: ["db"],
+        },
+      },
+    },
+  },
+});
