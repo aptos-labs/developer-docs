ef09ea45f52033ea0f2457a872a83511f241a5fd
Jackson <jackson.mills@devdocs.work>
2024-04-12 14:51:46 -0700

    Add multi_agent.js

--- apps/docusaurus/docs/reference/multi_agent.js
+++ apps/docusaurus/docs/reference/multi_agent.js
@@ -19,10 +19,12 @@ const example = async () => {
 
     let alice = Account.generate();
     let bob = Account.generate();
+    let carol = Account.generate();
 
     console.log("=== Addresses ===\n");
     console.log(`Alice's address is: ${alice.accountAddress}`);
     console.log(`Bob's address is: ${bob.accountAddress}`);
+    console.log(`Carol's address is: ${carol.accountAddress}`);
 
     console.log("\n=== Funding accounts ===\n");
     await aptos.fundAccount({
@@ -33,41 +35,53 @@ const example = async () => {
         accountAddress: bob.accountAddress,
         amount: 100,
     });
+    await aptos.fundAccount({
+        accountAddress: carol.accountAddress,
+        amount: 100,
+    });
     console.log("Done funding accounts.")
 
     // 1. Build
     console.log("\n=== 1. Building the transaction ===\n");
-    const transaction = await aptos.transaction.build.simple({
+    const transaction = await aptos.transaction.build.multiAgent({
         sender: alice.accountAddress,
+        secondarySignerAddresses: [bob.accountAddress],
         data: {
-        // All transactions on Aptos are implemented via smart contracts.
-        function: "0x1::coin::transfer",
-        typeArguments: [parseTypeTag("0x1::aptos_coin::AptosCoin")],
-        functionArguments: [bob.accountAddress, 100],
+            // All transactions on Aptos are implemented via smart contracts.
+            function: "0x1::coin::transfer",
+            typeArguments: [parseTypeTag("0x1::aptos_coin::AptosCoin")],
+            functionArguments: [carol.accountAddress, 100],
         },
     });
     console.log("Transaction:", transaction)
 
     // 2. Simulate (Optional)
-    const [userTransactionResponse] = await aptos.transaction.simulate.simple({
+    const [userTransactionResponse] = await aptos.transaction.simulate.multiAgent({
         signerPublicKey: alice.publicKey,
+        secondarySignersPublicKeys: [bob.publicKey],
         transaction,
     });
     console.log("\n === 2. Simulated Response === \n")
     console.log(userTransactionResponse)
 
     // 3. Sign
-    const senderAuthenticator = aptos.transaction.sign({
+    const aliceSenderAuthenticator = aptos.transaction.sign({
         signer: alice,
         transaction,
     });
+    const bobSenderAuthenticator = aptos.transaction.sign({
+        signer: bob,
+        transaction
+    })
     console.log("\n=== 3. Signed transaction ===\n");
-    console.log(senderAuthenticator)
+    console.log(aliceSenderAuthenticator)
+    console.log(bobSenderAuthenticator)
 
     // 4. Submit
-    const committedTransaction = await aptos.transaction.submit.simple({
+    const committedTransaction = await aptos.transaction.submit.multiAgent({
         transaction,
-        senderAuthenticator,
+        senderAuthenticator: aliceSenderAuthenticator,
+        additionalSignersAuthenticators: [bobSenderAuthenticator],
     });
 
     console.log("\n=== 4. Submitted transaction hash ===\n");
