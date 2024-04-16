5faffe95b66ed0afce17896ee6751f7ba24a7903
Jackson <jackson.mills@devdocs.work>
2024-04-15 17:52:11 -0700

    Update sponsor.js to properly simulate

--- apps/docusaurus/docs/reference/sponsor.js
+++ apps/docusaurus/docs/reference/sponsor.js
@@ -6,7 +6,6 @@ const {
     Account,
     Aptos,
     AptosConfig,
-    parseTypeTag,
     Network,
 } = require("@aptos-labs/ts-sdk");
 
@@ -14,7 +13,7 @@ const example = async () => {
     console.log("This example will create two accounts (Alice and Bob) and send a transaction transfering APT to Carol's account.");
 
     // 0. Setup the client and test accounts
-    const config = new AptosConfig({ network: Network.DEVNET });
+    const config = new AptosConfig({ network: Network.TESTNET });
     const aptos = new Aptos(config);
 
     let alice = Account.generate();
@@ -39,7 +38,7 @@ const example = async () => {
         accountAddress: carol.accountAddress,
         amount: 100,
     });
-    console.log("Done funding accounts.")
+    console.log("Funded the accounts!")
 
     // 1. Build
     console.log("\n=== 1. Building the transaction ===\n");
@@ -48,34 +47,32 @@ const example = async () => {
         withFeePayer: true,
         data: {
             // All transactions on Aptos are implemented via smart contracts.
-            function: "0x1::coin::transfer",
-            typeArguments: [parseTypeTag("0x1::aptos_coin::AptosCoin")],
+            function: "0x1::aptos_account::transfer",
             functionArguments: [carol.accountAddress, 100],
         },
     });
-    console.log("Transaction:", transaction)
+    console.log("Built the transaction!")
 
-    // 2. Simulate (Optional)
-    console.log("\n === 2. Simulating Response (Optional) === \n")
+    // 2. Sign
+    console.log("\n=== 2. Signing transaction ===\n");
+    const aliceSenderAuthenticator = aptos.transaction.sign({
+        signer: alice,
+        transaction,
+    });
+    const bobSenderAuthenticator = aptos.transaction.signAsFeePayer({
+        signer: bob,
+        transaction
+    })
+    console.log("Signed the transaction!")
+
+    // 3. Simulate (Optional)
+    console.log("\n === 3. Simulating Response (Optional) === \n")
     const [userTransactionResponse] = await aptos.transaction.simulate.simple({
         signerPublicKey: alice.publicKey,
         feePayerPublicKey: bob.publicKey,
         transaction,
     });
     console.log(userTransactionResponse)
-
-    // 3. Sign
-    console.log("\n=== 3. Signing transaction ===\n");
-    const aliceSenderAuthenticator = aptos.transaction.sign({
-        signer: alice,
-        transaction,
-    });
-    const bobSenderAuthenticator = aptos.transaction.sign({
-        signer: bob,
-        transaction
-    })
-    console.log(aliceSenderAuthenticator)
-    console.log(bobSenderAuthenticator)
 
     // 4. Submit
     console.log("\n=== 4. Submitting transaction ===\n");
@@ -84,12 +81,12 @@ const example = async () => {
         senderAuthenticator: aliceSenderAuthenticator,
         feePayerAuthenticator: bobSenderAuthenticator,
     });
-    console.log("Submitted Transaction");
+    console.log("Submitted transaction hash:", committedTransaction.hash);
 
     // 5. Wait for results
     console.log("\n=== 5. Waiting for result of transaction ===\n");
-    const result = await aptos.waitForTransaction({ transactionHash: committedTransaction.hash });
-    console.log(result)
+    const executedTransaction = await aptos.waitForTransaction({ transactionHash: committedTransaction.hash });
+    console.log(executedTransaction)
 };
 
 example();
