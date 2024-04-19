edf86d6e08c96329641e3c5224bdaa91da7f0185
Jackson <jackson.mills@devdocs.work>
2024-04-19 13:53:36 -0700

    Add move tutorials again

---
+++ apps/nextra/pages/en/build/cli/use-cli/working-with-move-contracts/_meta.tsx
@@ -0,0 +1,8 @@
+export default {
+  "arguments-in-json-tutorial": {
+    title: "Arguments in JSON Tutorial",
+  },
+  "multi-signature-tutorial": {
+    title: "Multisig Governance Tutorial",
+  },
+};
