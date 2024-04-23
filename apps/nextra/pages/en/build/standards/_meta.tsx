705c51e9ce00ec8144629bffa4e62faa56f8889a
Greg Nazario <greg@gnazar.io>
2024-04-23 13:56:11 -0700

    [nextra] Migrate standards section

---
+++ apps/nextra/pages/en/build/standards/_meta.tsx
@@ -0,0 +1,9 @@
+export default {
+  index: "Introduction",
+  "aptos-object": "Aptos Object",
+  "digital-asset": "Digital Asset (DA)",
+  "fungible-asset": "Fungible Asset (FA)",
+  wallets: "Wallet",
+  "aptos-coin": "Aptos Coin (legacy)",
+  "aptos-token": "Aptos Token (legacy)",
+};
