0779ff400c913fc31840491dfc68c1e881e4360f
Jackson <jackson.mills@devdocs.work>
2024-04-23 14:44:11 -0700

    Add _meta.tsx files

--- apps/nextra/pages/en/build/indexer/_meta.tsx
+++ apps/nextra/pages/en/build/indexer/_meta.tsx
@@ -5,5 +5,17 @@ export default {
       toc: false,
       layout: "full",
     },
+  }, 
+  "api": {
+    title: "Indexer API"
+  },
+  "custom-processors": {
+    title: "Custom Processors",
+  },
+  "txn-stream": {
+    title: "Transaction Stream Service",
+  },
+  "legacy": {
+    title: "Legacy Indexer",
   },
 };
