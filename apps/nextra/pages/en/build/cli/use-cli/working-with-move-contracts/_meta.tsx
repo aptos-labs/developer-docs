b1c9d041c528e08bacf781b6e0f72b07500ad396
Victor Gao <vgao1996@gmail.com>
2024-04-24 23:54:06 +0000

    Add docs for local simulation, benchmarking and gas profiling

--- apps/nextra/pages/en/build/cli/use-cli/working-with-move-contracts/_meta.tsx
+++ apps/nextra/pages/en/build/cli/use-cli/working-with-move-contracts/_meta.tsx
@@ -1,4 +1,7 @@
 export default {
+  "local-simulation-benchmarking-and-gas-profiling": {
+    title: "Local Simulation, Benchmarking & Gas Profiling",
+  },
   "arguments-in-json-tutorial": {
     title: "Arguments in JSON Tutorial",
   },
