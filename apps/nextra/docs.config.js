c4bdf47f184548c66ba5cf626de655f2b03365c2
hariria <hariria@usc.edu>
2024-04-26 16:30:13 -0700

    [nextra] Add default origin in case .env doesn't exist

--- apps/nextra/docs.config.js
+++ apps/nextra/docs.config.js
@@ -1,6 +1,18 @@
 /* eslint-disable no-undef */
 // Unfortunately making this a .ts file is not easily doable atm
 // TODO: revisit this if time allows
+
+const getOrigin = () => {
+  if (process.env.NEXT_PUBLIC_ORIGIN) {
+    return process.env.NEXT_PUBLIC_ORIGIN;
+  } else {
+    console.warn(
+      "\x1b[33mWarning:\x1b[0m [nextra] No .env with NEXT_PUBLIC_ORIGIN found in apps/nextra. Using default http://localhost:3030\n",
+    );
+    return "http://localhost:3030";
+  }
+};
+
 export const i18nConfig = Object.freeze({
   en: {
     locale: "en",
@@ -44,5 +56,5 @@ export const docsConfig = Object.freeze({
   relativeDocsPath: "/apps/nextra",
   githubNewIssueUrl: "https://github.com/aptos-labs/developer-docs/issues/new",
   googleAnalyticsId: "G-LLF79THJN0",
-  origin: process.env.NEXT_PUBLIC_ORIGIN,
+  origin: getOrigin(),
 });
