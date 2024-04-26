b5ad1e4ba31030ffa8075a15f65cddf189188fb1
Andrew Hariri <hariria@usc.edu>
2024-04-26 14:10:05 -0700

    [nextra] Create generic Codeblock component

--- apps/nextra/components/index.tsx
+++ apps/nextra/components/index.tsx
@@ -3,4 +3,5 @@ export * from "./tabs";
 export * from "./themed-image";
 export * from "./graphql";
 export * from "./beta-notice";
+export * from './codeblock';
 export * from "./remote-codeblock";
