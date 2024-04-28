d0c7608f87aaeb42df3355b0cb5fde2bc531c954
Jackson <jackson.mills@devdocs.work>
2024-04-28 13:27:28 -0700

    Fix most issues in building pages

---
+++ apps/nextra/pages/en/build/sdks/ts-sdk/building-transactions/_meta.tsx
@@ -0,0 +1,14 @@
+export default {
+    "multi-agent-signatures": {
+        title: "Multi-Agent Signatures",
+    },
+    "sponsoring-transactions": {
+        title: "Sponsoring Transactions",
+    },
+    "batching-transactions": {
+        title: "Batching Transactions",
+    },
+    "bcs-format": {
+        title: "BCS Format",
+    },
+};
