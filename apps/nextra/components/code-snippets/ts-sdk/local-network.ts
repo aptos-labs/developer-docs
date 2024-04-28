0d96084dfc09d5f06decd9bd5cd2f390881cb463
Jackson <jackson.mills@devdocs.work>
2024-04-28 06:37:45 -0700

    pnpm fmt

--- apps/nextra/components/code-snippets/ts-sdk/local-network.ts
+++ apps/nextra/components/code-snippets/ts-sdk/local-network.ts
@@ -1,24 +1,24 @@
-import { Aptos, AptosConfig, Network} from "@aptos-labs/ts-sdk";
+import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
 import { LocalNode } from "@aptos-labs/ts-sdk/dist/common/cli/index.js";
-  
+
 async function example() {
-    // Initiate a LocalNode instance
-    const localNode = new LocalNode();
+  // Initiate a LocalNode instance
+  const localNode = new LocalNode();
 
-    // Run a local node
-    await localNode.run();
+  // Run a local node
+  await localNode.run();
 
-    // // Write some tests
-    // async function test() {
-    //     await localNode.waitUntilProcessIsUp();
-    //     const aptosConfig = new AptosConfig({ network: Network.LOCAL });
-    //     const aptos = new Aptos(aptosConfig);
-    //     // rest of test.....
-    // }
-    // await test()
+  // // Write some tests
+  // async function test() {
+  //     await localNode.waitUntilProcessIsUp();
+  //     const aptosConfig = new AptosConfig({ network: Network.LOCAL });
+  //     const aptos = new Aptos(aptosConfig);
+  //     // rest of test.....
+  // }
+  // await test()
 
-    // // Stop the local node
-    // await localNode.stop();
+  // // Stop the local node
+  // await localNode.stop();
 }
 
-example()
+example();
