17877f0b54fae6fa9022c78aaecfdb19453ac4eb
Jackson <jackson.mills@devdocs.work>
2024-04-28 20:40:15 -0700

    Fix most component usages

--- apps/nextra/pages/en/build/sdks/ts-sdk/_meta.tsx
+++ apps/nextra/pages/en/build/sdks/ts-sdk/_meta.tsx
@@ -1,3 +1,11 @@
 export default {
-
+    "fetch-data-via-sdk": {
+        title: "Fetch Data via the TypeScript SDK"
+    },
+    "building-transactions": {
+        title: "Building and Sending Transactions"
+    },
+    "legacy-sdk": {
+        title: "Legacy TypeScript SDK"
+    }
 };
