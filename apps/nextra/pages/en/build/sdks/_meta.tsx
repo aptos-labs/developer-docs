75b22f14ed44467d3c0e676d972b85e79392c38f
Jackson <163934542+jmintuitive@users.noreply.github.com>
2024-04-29 16:20:56 -0700

    SDK Migration (#291)
    * Add the first layer of pages
    
    * Update quickstart file
    
    * Create example code
    
    * pnpm fmt
    
    * Add legacy ts pages with titles
    
    * Fix most legacy relative links
    
    * Replace special dash with normal dash
    
    * Use Legacy SDK consistently in legacy docs
    
    * Fix most issues in building pages
    
    * Fix unneeded todos
    
    * Fix most component usages
    
    * Fix absolute links to relative links
    
    * Add comma
    
    * Remove code samples
    
    * Fixed build issue due to typod meta file
    
    * Fix ts-sdk meta.tsx
    
    * Add codeblock filenames
    
    * Update building-transactions content
    
    * Fix multi-agent-transactions meta file
    
    * Remove all instances of "we"
    
    * Update multi-agent-transactions to use steps
    
    * Fix content for Sponsoring Transactions
    
    * Fix batching-transactions content
    
    * Fix bcs-format content
    
    * Fix missing Legacy in legacy section
    
    * Fix other SDK page content
    
    * Add account.mdx
    
    * Fix TODO
    
    * Add callout for specific fullnode network
    
    * Fix content for ts-sdk quickstart
    
    * pnpm fmt
    
    * Fix ts-sdk quickstart content part 2
    
    * Fix external links to SDKs
    
    * Fix navigation to match live for legacy pages
    
    * Use ts instead of tsx
    
    * Simplify Aptos _ SDK -> _ SDK
    
    * Use npm2yarn
    
    * Remove the slugs

---
+++ apps/nextra/pages/en/build/sdks/_meta.tsx
@@ -0,0 +1,14 @@
+export default {
+  "ts-sdk": {
+    title: "TypeScript SDK",
+  },
+  "python-sdk": {
+    title: "Python SDK",
+  },
+  "rust-sdk": {
+    title: "Rust SDK",
+  },
+  "unity-sdk": {
+    title: "Unity SDK",
+  },
+};
