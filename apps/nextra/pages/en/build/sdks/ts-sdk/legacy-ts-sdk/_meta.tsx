c86cc9f1252783b49ffad208315363a3c66e3893
Jackson <jackson.mills@devdocs.work>
2024-04-29 15:28:01 -0700

    Fix navigation to match live for legacy pages

--- apps/nextra/pages/en/build/sdks/ts-sdk/legacy-ts-sdk/_meta.tsx
+++ apps/nextra/pages/en/build/sdks/ts-sdk/legacy-ts-sdk/_meta.tsx
@@ -1,15 +1,12 @@
 export default {
-  "aptos-client": {
-    title: "AptosClient Class",
-  },
-  "indexer-client": {
-    title: "IndexerClient Class",
-  },
   "migration-guide": {
     title: "TypeScript SDK Migration Guide",
   },
+  "typescript-sdk-overview": {
+    title: "Legacy TypeScript SDK Architecture",
+  },
   "sdk-client-layer": {
-    title: "SDK Client Layer",
+    title: "API Client Layer",
   },
   "sdk-core-layer": {
     title: "Core Layer",
@@ -19,8 +16,5 @@ export default {
   },
   "sdk-tests": {
     title: "Tests and Validation",
-  },
-  "typescript-sdk-overview": {
-    title: "Legacy TypeScript SDK Architecture",
   },
 };
