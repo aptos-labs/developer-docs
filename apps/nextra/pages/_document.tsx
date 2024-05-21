b086135da02f77b24632a865911a46b541654a56
Andrew Hariri <hariria@usc.edu>
2024-05-21 09:43:10 -0700

    [nextra] Fix hotjar again

--- apps/nextra/pages/_document.tsx
+++ apps/nextra/pages/_document.tsx
@@ -10,18 +10,15 @@ class MyDocument extends Document {
           <Script
             dangerouslySetInnerHTML={{
               __html: `
-            <!-- Hotjar Tracking Code for my-hotjar-app -->
-            <script>
                 (function(h,o,t,j,a,r){
-                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                    h._hjSettings={hjid:3921748,hjsv:6};
-                    a=o.getElementsByTagName('head')[0];
-                    r=o.createElement('script');r.async=1;
-                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                    a.appendChild(r);
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:3921748,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
                 })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            </script>
-          `,
+              `,
             }}
           />
         </Head>
