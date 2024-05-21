bec81150eacbbfbe2959f4cbd299fb8889ba1600
hariria <hariria@usc.edu>
2024-05-21 08:59:50 -0700

    [nextra] Hotjar script fix

--- apps/nextra/pages/_document.tsx
+++ apps/nextra/pages/_document.tsx
@@ -12,14 +12,14 @@ class MyDocument extends Document {
               __html: `
             <!-- Hotjar Tracking Code for my-hotjar-app -->
             <script>
-              (function(h,o,t,j,a,r){
-                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                  h._hjSettings={hjid:1234567,hjsv:6};
-                  a=o.getElementsByTagName('head')[0];
-                  r=o.createElement('script');r.async=1;
-                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                  a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:3921748,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
             </script>
           `,
             }}
