b119d99e765a896f32141eb3092ea9932515bf43
hariria <hariria@usc.edu>
2024-05-24 09:55:00 -0700

    [nextra] Getting started changes

--- apps/nextra/pages/en/build/get-started/_meta.tsx
+++ apps/nextra/pages/en/build/get-started/_meta.tsx
@@ -1,6 +1,6 @@
 export default {
   "create-aptos-dapp": {
-    title: "create-aptos-dapp GitHub",
+    title: "create-aptos-dapp",
     href: "https://github.com/aptos-labs/create-aptos-dapp",
   },
 };
