b1f50bdd1aebcb77774b34786c0611d5f134eba6
Kshitij Chakvarty <chakravartykshitij@gmail.com>
2024-05-29 15:21:12 -0700

    fetching languages from docs.config

--- apps/nextra/generate-sitemap-index.cjs
+++ apps/nextra/generate-sitemap-index.cjs
@@ -18,12 +18,9 @@ const generateSitemapIndex = async () => {
   const availableLanguages = await getAvailableLanguages();
   
   const sitemapIndexContent = `<?xml version="1.0" encoding="UTF-8"?>
-  <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${availableLanguages.map(lang => `
-      <sitemap>
-        <loc>https://preview.aptos.dev/sitemap-${lang}.xml</loc>
-      </sitemap>`).join('')}
-  </sitemapindex>`;
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${availableLanguages.map(lang => `<sitemap><loc>https://preview.aptos.dev/sitemap-${lang}.xml</loc></sitemap>`).join('')}
+</sitemapindex>`;
 
   fs.writeFileSync(path.resolve(outputDir, 'sitemap.xml'), sitemapIndexContent);
   console.log('Index sitemap written to public/sitemap.xml');
