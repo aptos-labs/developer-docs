185b5db1367a8db876d7363c653ebb314a1a578e
Jackson <163934542+jmintuitive@users.noreply.github.com>
2024-05-30 15:34:01 -0400

    Migrate remaining tutorials (#369)
    * Move tutorials over
    
    * Fix tabs
    
    * re-add static
    
    * Update first-coin.mdx
    
    * Replace all code snippets in top-tutorials
    
    * Fix Tabs usage for building-from-source
    
    * Add all tutorials
    
    * Fix all links to the tutorials
    
    * Comment out links which are dead
    
    * Add redirects for the tutorials
    
    * pnpm fmt
    
    * Remove TODO
    
    * Remove advanced-guides from meta to match main

---
+++ apps/nextra/pages/en/build/tutorials/build-e2e-dapp/_meta.tsx
@@ -0,0 +1,20 @@
+export default {
+  "1-create-smart-contract": {
+    title: "1. Create Smart Contract",
+  },
+  "2-set-up-react-app": {
+    title: "2. Set Up React App",
+  },
+  "3-add-wallet-support": {
+    title: "3. Add Wallet Support",
+  },
+  "4-fetch-data-from-chain": {
+    title: "4. Fetch Data from Chain",
+  },
+  "5-submit-data-to-chain": {
+    title: "5. Submit Data to Chain",
+  },
+  "6-handle-tasks": {
+    title: "6. Handle Tasks",
+  },
+};
