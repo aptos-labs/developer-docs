4c7a2acba5fd0a8972acd5d12357be1d84578f9d
Andrew Hariri <hariria@usc.edu>
2024-06-01 15:20:50 -0700

    [nextra] fix href absolute path (#384)

--- apps/nextra/pages/en/build/apis/_meta.tsx
+++ apps/nextra/pages/en/build/apis/_meta.tsx
@@ -2,7 +2,7 @@ export default {
   "fullnode-rest-api": "Fullnode REST API",
   "indexer-graphql-api": {
     title: "Indexer GraphQL API",
-    href: "./indexer",
+    href: "/en/build/indexer",
   },
   "aptos-labs-developer-portal": "Developer Portal",
 };
