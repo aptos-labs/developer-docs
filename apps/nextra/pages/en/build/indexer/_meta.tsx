01ac44d9d1a4c72396fd5934a99be8a71952a533
hariria <hariria@usc.edu>
2024-06-10 12:01:32 -0700

    [nextra] Flatten indexer content

--- apps/nextra/pages/en/build/indexer/_meta.tsx
+++ apps/nextra/pages/en/build/indexer/_meta.tsx
@@ -1,9 +1,10 @@
 export default {
+  "---concepts---": {
+    type: "separator",
+    title: "Concepts",
+  },
   "indexer-reference": {
     title: "Indexer API Table Reference",
-  },
-  "common-queries": {
-    title: "Common Indexer Queries",
   },
   architecture: {
     title: "Architecture",
@@ -16,6 +17,70 @@ export default {
   },
   "txn-stream": {
     title: "Transaction Stream Service",
+  },
+  "---example-queries---": {
+    type: "separator",
+    title: "Example Queries",
+  },
+  "coin-balances": {
+    title: "Get Coin Balances",
+    theme: {
+      toc: false,
+      layout: "full",
+    },
+  },
+  "account-transactions": {
+    title: "Get Transactions Impacting an Account",
+    theme: {
+      toc: false,
+      layout: "full",
+    },
+  },
+  "ans-lookup": {
+    title: "Get Account Domain Name",
+    theme: {
+      toc: false,
+      layout: "full",
+    },
+  },
+  "coin-info": {
+    title: "Get Coin Info",
+    theme: {
+      toc: false,
+      layout: "full",
+    },
+  },
+  "get-nft-collections": {
+    title: "Get NFT Collections By Account",
+    theme: {
+      toc: false,
+      layout: "full",
+    },
+  },
+  "get-nfts": {
+    title: "Get NFTs By Account",
+    theme: {
+      toc: false,
+      layout: "full",
+    },
+  },
+  "token-metadata": {
+    title: "Get Token Metadata",
+    theme: {
+      toc: false,
+      layout: "full",
+    },
+  },
+  "get-delegators": {
+    title: "Count Delegators in Staking Pool",
+    theme: {
+      toc: false,
+      layout: "full",
+    },
+  },
+  "---legacy---": {
+    type: "separator",
+    title: "Legacy",
   },
   legacy: {
     title: "Legacy Indexer",
