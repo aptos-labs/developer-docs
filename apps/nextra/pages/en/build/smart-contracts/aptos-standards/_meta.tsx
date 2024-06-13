a38a9b5e8ffe13d6db3f67d03ef5e8d60e9d5d9e
hariria <hariria@usc.edu>
2024-06-13 16:44:04 -0700

    [nextra] Restructuring Move

--- apps/nextra/pages/en/build/smart-contracts/aptos-standards/_meta.tsx
+++
@@ -1,8 +0,0 @@
-export default {
-  objects: "Object",
-  tokens: "Aptos Token Standard Overview",
-  "digital-asset": "Digital Asset (DA)",
-  "fungible-asset": "Fungible Asset (FA)",
-  "aptos-coin": "Coin (legacy)",
-  "aptos-token": "Token (legacy)",
-};
