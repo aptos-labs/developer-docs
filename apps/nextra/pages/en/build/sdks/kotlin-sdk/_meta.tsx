5d95ae90961e40f60ae53ec7e7f63b81316f235f
astinz <28899947+astinz@users.noreply.github.com>
2024-06-17 14:45:32 +0300

    add Kotlin SDK docs...

---
+++ apps/nextra/pages/en/build/sdks/kotlin-sdk/_meta.tsx
@@ -0,0 +1,17 @@
+export default {
+  quickstart: {
+    title: "Quickstart",
+  },
+  "---": {
+    type: "separator",
+  },
+  "fetch-data-via-sdk": {
+    title: "Fetch Data via the Kotlin SDK",
+  },
+  "building-transactions": {
+    title: "Building and Sending Transactions",
+  },
+  account: {
+    title: "Creating and Managing Accounts",
+  },
+};
