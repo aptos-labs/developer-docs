60aefa42517fdbff46216630396c6d009123aad1
Oliver <heliuchuan@gmail.com>
2024-06-24 18:48:51 -0400

    Update titles

--- apps/nextra/pages/en/build/guides/aptos-keyless/_meta.tsx
+++ apps/nextra/pages/en/build/guides/aptos-keyless/_meta.tsx
@@ -1,13 +1,18 @@
 export default {
     "introduction": {
+      title: "Introduction",
     },
     "oidc-support": {
+      title: "OIDC Support",
     },
     "integration-guide": {
+      title: "Integration Guide",
     },
     "how-keyless-works": {
+      title: "How Keyless Works",
     },
     "other": {
+      title: "Terminology and FAQ",
     },
   };
   
