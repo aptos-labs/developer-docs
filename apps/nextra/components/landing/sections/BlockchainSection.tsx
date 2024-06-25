d2b4d29a9087ca1568db0933cb9a142629d9ec8b
hariria <hariria@usc.edu>
2024-06-25 15:11:28 -0700

    [nextra] Change from drand randomness link to aptos roll

--- apps/nextra/components/landing/sections/BlockchainSection.tsx
+++ apps/nextra/components/landing/sections/BlockchainSection.tsx
@@ -60,7 +60,7 @@ export function BlockchainSection() {
         <FeatureCard
           label={t.randomnessLabel}
           description={t.randomnessDescription}
-          href={`/${locale}/build/smart-contracts/cryptography#verifying-randomness-from-the-drand-beacon`}
+          href={`/${locale}/build/smart-contracts/randomness`}
         />
         <FeatureCard
           label={t.feePayerLabel}
