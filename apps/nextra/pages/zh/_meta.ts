31868cd3fac24272d19394050a1a6c22d3616dff
WGB5445 <919603023@qq.com>
2024-06-27 14:02:09 +0800

    update new Zh meta

--- apps/nextra/pages/zh/_meta.ts
+++ apps/nextra/pages/zh/_meta.ts
@@ -11,29 +11,31 @@ export default {
   build: {
     type: "menu",
     title: "Build",
-    items: {
-      // "quick-start": {
-      //   title: "Quick Start",
-      // },
-      // "smart-contracts": {
-      //   title: "Smart Contracts",
-      // },
-      // apis: {
-      //   title: "APIs",
-      // },
-      // sdks: {
-      //   title: "SDKs",
-      // },
-      // indexer: {
-      //   title: "Indexer",
-      // },
-      cli: {
-        title: "CLI",
-      },
-      // "advanced-guides": {
-      //   title: "Advanced Guides",
-      // },
+    // items: {
+    //   "get-started": {
+    //     title: "Get Started",
+    //   },
+    //   "smart-contracts": {
+    //     title: "Smart Contracts (Move)",
+    //   },
+    //   apis: {
+    //     title: "APIs",
+    //   },
+    //   sdks: {
+    //     title: "SDKs",
+    //   },
+    //   indexer: {
+    //     title: "Indexer",
+    //   },
+    cli: {
+      title: "CLI",
     },
+    // "create-aptos-dapp": {
+    //   title: "create-aptos-dapp",
+    // },
+    // guides: {
+    //   title: "Guides",
+    // },
   },
   network: {
     type: "menu",
@@ -41,6 +43,46 @@ export default {
     items: {
       nodes: {
         title: "Nodes",
+      },
+    },
+  },
+  "developer-platforms": {
+    type: "menu",
+    title: "Additional Resources",
+    items: {
+      learn: {
+        type: "page",
+        title: "Aptos Learn ↗",
+        href: "https://learn.aptoslabs.com/",
+        newWindow: true,
+      },
+      forum: {
+        title: "Community Forum ↗",
+        href: "https://forum.aptosfoundation.org/",
+        newWindow: true,
+        type: "page",
+      },
+      discussions: {
+        title: "Developer Discussions ↗",
+        href: "https://github.com/aptos-labs/aptos-developer-discussions/discussions",
+        newWindow: true,
+        type: "page",
+      },
+      api_gateway: {
+        title: "API Gateway ↗",
+        href: "https://developers.aptoslabs.com/",
+        newWindow: true,
+        type: "page",
+      },
+      aptos_foundation: {
+        title: "Aptos Foundation ↗",
+        href: "https://aptosfoundation.org/",
+        newWindow: true,
+        type: "page",
+      },
+      contribute: {
+        title: "Contribute",
+        type: "page",
       },
     },
   },
