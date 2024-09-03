c8f186ff753d1395e076122a6457492a82670281
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2024-09-02 23:32:37 -0700

    Update _meta.tsx

--- apps/nextra/pages/en/build/create-aptos-dapp/templates/_meta.tsx
+++ apps/nextra/pages/en/build/create-aptos-dapp/templates/_meta.tsx
@@ -2,6 +2,12 @@ export default {
   boilerplate: {
     title: "Boilerplate Template",
   },
+  "tg-mini-app-boilerplate": {
+    title: "Telegram Mini App Boilerplate",
+  },
+  "tg-mini-app-seamless-signing-boilerplate": {
+    title: "Telegram Mini App Supports Seamless Signing Boilerplate",
+  },
   "nft-minting-dapp": {
     title: "NFT minting dapp Template",
   },
