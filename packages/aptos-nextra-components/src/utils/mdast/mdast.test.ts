a258735812e5866fa66d0f9d1e4e8777da1c0a09
hariria <hariria@usc.edu>
2024-09-05 16:47:15 -0700

    [mdast] account.md

--- packages/aptos-nextra-components/src/utils/mdast/mdast.test.ts
+++ packages/aptos-nextra-components/src/utils/mdast/mdast.test.ts
@@ -1,10 +1,17 @@
 import { expect, test } from 'vitest'
-import { astToMarkdown, convertHtmlToMarkdownCodeBlocks, readFileAsTree } from './mdast'
+import { astToMarkdown, convertHtmlToMarkdownCodeBlocks, readFileAsTree, writeFile } from './mdast'
 
-test('keyRotation parse and fix AST', () => {
+test('keyRotation.md parse and fix AST', () => {
   const tree = readFileAsTree("./examples/keyRotation.md");
   convertHtmlToMarkdownCodeBlocks(tree);
   const markdown = astToMarkdown(tree);
-  console.log(markdown);
   expect(markdown).toEqual(astToMarkdown(readFileAsTree('./examples/keyRotation.expect.md')).toString())
+})
+
+test('account.md parse and fix AST', () => {
+  const tree = readFileAsTree("./examples/account.md");
+  convertHtmlToMarkdownCodeBlocks(tree);
+  const markdown = astToMarkdown(tree);
+  console.log(markdown)
+  expect(markdown).toEqual(astToMarkdown(readFileAsTree('./examples/account.expect.md')).toString())
 })
