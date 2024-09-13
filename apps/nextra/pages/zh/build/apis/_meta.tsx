5cfc0fb187c0e4eb42ca92af02a4d2a40a8c9c35
WGB5445 <919603023@qq.com>
2024-09-13 21:10:01 +0800

    Refactor Chinese translation for "Get Started" page and APIs

---
+++ apps/nextra/pages/zh/build/apis/_meta.tsx
@@ -0,0 +1,20 @@
+export default {
+  "fullnode-rest-api-reference": {
+    title: "Fullnode REST API Reference",
+    theme: {
+      toc: false,
+      layout: "full",
+      sidebar: false,
+    },
+  },
+  "fullnode-rest-api": "Fullnode REST API 教程",
+  "indexer-graphql-api": {
+    title: "Indexer GraphQL API",
+    href: "/zh/build/indexer",
+  },
+  "data-providers": "Data Providers",
+  "faucet-api": {
+    title: "Faucet API",
+  },
+  "aptos-labs-developer-portal": "API Gateway",
+};
