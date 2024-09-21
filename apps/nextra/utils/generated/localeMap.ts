195ab12dbd14942597caadf67c545f2d7379f301
Teng Zhang <rahxephon89@163.com>
2024-09-21 16:19:40 -0700

    revert

--- apps/nextra/utils/generated/localeMap.ts
+++ apps/nextra/utils/generated/localeMap.ts
@@ -4,28 +4,22 @@
 
 export const localeMap = {
   "/build/apis/aptos-labs-developer-portal": {
-    "en": true,
-    "zh": true
+    "en": true
   },
   "/build/apis/data-providers": {
-    "en": true,
-    "zh": true
+    "en": true
   },
   "/build/apis/faucet-api": {
-    "en": true,
-    "zh": true
+    "en": true
   },
   "/build/apis/fullnode-rest-api-reference": {
-    "en": true,
-    "zh": true
+    "en": true
   },
   "/build/apis/fullnode-rest-api": {
-    "en": true,
-    "zh": true
+    "en": true
   },
   "/build/apis": {
-    "en": true,
-    "zh": true
+    "en": true
   },
   "/build/cli/install-cli/install-cli-linux": {
     "en": true,
@@ -137,8 +131,7 @@ export const localeMap = {
     "en": true
   },
   "/build/get-started": {
-    "en": true,
-    "zh": true
+    "en": true
   },
   "/build/guides/aptos-keyless/how-keyless-works": {
     "en": true
@@ -581,6 +574,9 @@ export const localeMap = {
   "/build/smart-contracts/objects": {
     "en": true
   },
+  "/build/smart-contracts/pokemon/[name]": {
+    "en": true
+  },
   "/build/smart-contracts/prover/prover-guide": {
     "en": true
   },
@@ -757,9 +753,6 @@ export const localeMap = {
     "en": true
   },
   "/network/blockchain/events": {
-    "en": true
-  },
-  "/network/blockchain/execution": {
     "en": true
   },
   "/network/blockchain/fullnodes": {
