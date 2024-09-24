05ecfcc7406b28bed0e1a94544f0e091e754373b
Luke Cassady-Dorion <luke@SPStories.com>
2024-09-24 15:40:12 +0700

    Updated Irys to use latest tooling / messaging

--- apps/nextra/utils/generated/localeMap.ts
+++ apps/nextra/utils/generated/localeMap.ts
@@ -4,21 +4,30 @@
 
 export const localeMap = {
   "/build/apis/aptos-labs-developer-portal": {
-    "en": true
+    "en": true,
+    "zh": true
   },
   "/build/apis/data-providers": {
-    "en": true
+    "en": true,
+    "zh": true
   },
   "/build/apis/faucet-api": {
-    "en": true
+    "en": true,
+    "zh": true
   },
   "/build/apis/fullnode-rest-api-reference": {
-    "en": true
+    "en": true,
+    "zh": true
   },
   "/build/apis/fullnode-rest-api": {
-    "en": true
+    "en": true,
+    "zh": true
   },
   "/build/apis": {
+    "en": true,
+    "zh": true
+  },
+  "/build/cli/formatting-move-contracts": {
     "en": true
   },
   "/build/cli/install-cli/install-cli-linux": {
@@ -131,7 +140,8 @@ export const localeMap = {
     "en": true
   },
   "/build/get-started": {
-    "en": true
+    "en": true,
+    "zh": true
   },
   "/build/guides/aptos-keyless/how-keyless-works": {
     "en": true
@@ -574,9 +584,6 @@ export const localeMap = {
   "/build/smart-contracts/objects": {
     "en": true
   },
-  "/build/smart-contracts/pokemon/[name]": {
-    "en": true
-  },
   "/build/smart-contracts/prover/prover-guide": {
     "en": true
   },
@@ -753,6 +760,9 @@ export const localeMap = {
     "en": true
   },
   "/network/blockchain/events": {
+    "en": true
+  },
+  "/network/blockchain/execution": {
     "en": true
   },
   "/network/blockchain/fullnodes": {
