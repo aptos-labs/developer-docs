47b27ee7018cdb919cdbf6da2c8abe96127c3c17
Philip Vu <pvu2002@outlook.com>
2024-09-27 15:13:18 -0700

    Add API reference and examples page

--- apps/nextra/pages/en/build/sdks/dotnet-sdk/_meta.tsx
+++ apps/nextra/pages/en/build/sdks/dotnet-sdk/_meta.tsx
@@ -14,4 +14,7 @@ export default {
   transactions: {
     title: "Transactions",
   },
+  "dotnet-examples": {
+    title: "Examples",
+  },
 };
--- apps/nextra/pages/en/build/sdks/dotnet-sdk/accounts/_meta.tsx
+++ apps/nextra/pages/en/build/sdks/dotnet-sdk/accounts/_meta.tsx
@@ -2,4 +2,10 @@ export default {
   ed25519: {
     title: "Ed25519 Accounts",
   },
+  keyless: {
+    title: "Keyless Accounts",
+  },
+  multikey: {
+    title: "Multikey Accounts",
+  },
 };
