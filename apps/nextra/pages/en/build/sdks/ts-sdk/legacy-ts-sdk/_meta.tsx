bb7a61c8d2cccdad7d66c63a4f5e8adba0d26c95
Greg Nazario <greg@gnazar.io>
2024-09-29 22:55:14 -0700

    [sdks] Remove legacy SDK docs except for migration

--- apps/nextra/pages/en/build/sdks/ts-sdk/legacy-ts-sdk/_meta.tsx
+++ apps/nextra/pages/en/build/sdks/ts-sdk/legacy-ts-sdk/_meta.tsx
@@ -2,19 +2,4 @@ export default {
   "migration-guide": {
     title: "TypeScript SDK Migration Guide",
   },
-  "typescript-sdk-overview": {
-    title: "Legacy TypeScript SDK Architecture",
-  },
-  "sdk-client-layer": {
-    title: "API Client Layer",
-  },
-  "sdk-core-layer": {
-    title: "Core Layer",
-  },
-  "sdk-plugins-layer": {
-    title: "Plugins Layer",
-  },
-  "sdk-tests": {
-    title: "Tests and Validation",
-  },
 };
