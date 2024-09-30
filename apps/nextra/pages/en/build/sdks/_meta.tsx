0fc0340d97cffdb9b0b1f613a8172df2508a08d5
Greg Nazario <greg@gnazar.io>
2024-09-29 23:04:30 -0700

    [sdks] Reorder to keep those with more information at the top

--- apps/nextra/pages/en/build/sdks/_meta.tsx
+++ apps/nextra/pages/en/build/sdks/_meta.tsx
@@ -2,14 +2,14 @@ export default {
   "ts-sdk": {
     title: "TypeScript SDK",
   },
-  "python-sdk": {
-    title: "Python SDK",
-  },
   "go-sdk": {
     title: "Go SDK",
   },
   "dotnet-sdk": {
     title: ".NET SDK",
+  },
+  "python-sdk": {
+    title: "Python SDK",
   },
   "cpp-sdk": {
     title: "C++ / Unreal SDK",
