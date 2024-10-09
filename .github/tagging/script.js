f4b455372a8b8e13109b19b4a440d2d6c4cfea4e
Igor Pokryshevskiy <igor.pokryshevskiy@aptoslabs.com>
2024-10-09 13:32:35 -0400

    move script into its own file

---
+++ .github/tagging/script.js
@@ -0,0 +1,23 @@
+const fs = require("fs")
+
+module.exports = async ({ inputSectionsStr, github, context }) => {
+  console.log("Running tagging script")
+  console.log(`Input: ${inputSectionsStr}`)
+
+  const sectionTagsString = fs.readFileSync("./.github/tagging/tagsByIssueSection.json")
+  const sectionTags = JSON.parse(sectionTagsString)
+  const inputSections = inputSectionsStr.split(",").map(s => s.trim())
+  const body = inputSections
+    .map(section => sectionTags[section])
+    .filter(Boolean)
+    .join(" ")
+
+  console.log(`Creating comment: ${body}`)
+  const result = await github.rest.issues.createComment({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    body
+  })
+  console.log("Result", result)
+}
