43290b684cf25d4478885273fe09266ee6164326
Igor Pokryshevskiy <igor.pokryshevskiy@aptoslabs.com>
2024-10-25 11:32:49 -0400

    update CAD template links to point to Aptos Learn

--- apps/nextra/pages/en/build/create-aptos-dapp/templates/_meta.tsx
+++ apps/nextra/pages/en/build/create-aptos-dapp/templates/_meta.tsx
@@ -1,20 +1,32 @@
 export default {
   boilerplate: {
     title: "Boilerplate Template",
+    href: "https://learn.aptoslabs.com/en/dapp-templates/boilerplate-template",
+    newWindow: true,
   },
   "clicker-game-tg-mini-app": {
     title: "Telegram Clicker Game Mini App Template",
+    href: "https://learn.aptoslabs.com/en/dapp-templates/telegram-mini-app-template",
+    newWindow: true,
   },
   "nft-minting-dapp": {
     title: "NFT minting dapp Template",
+    href: "https://learn.aptoslabs.com/en/dapp-templates/nft-minting-template",
+    newWindow: true,
   },
   "token-minting-dapp": {
     title: "Token minting dapp Template",
+    href: "https://learn.aptoslabs.com/en/dapp-templates/token-minting-template",
+    newWindow: true,
   },
   "token-staking-dapp": {
     title: "Token staking dapp Template",
+    href: "https://learn.aptoslabs.com/en/dapp-templates/token-staking-template",
+    newWindow: true,
   },
   "custom-indexer": {
     title: "Custom Indexer Template",
+    href: "https://learn.aptoslabs.com/en/dapp-templates/custom-indexer-template",
+    newWindow: true,
   },
 };
