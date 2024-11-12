46d3ac88816126ec26a99e32a76de7fca4ace93f
aldenhu <msmouse@gmail.com>
2024-11-12 22:15:26 +0000

    temp

---
+++ apps/nextra/pages/en/network/blockchain/data-structure-deep-dive/_meta.ts
@@ -0,0 +1,5 @@
+export default {
+  "core-data-model": "Core Data Model",
+  "authentication": "Authentication Data Structures",
+  "incentives": "Storage Incentives"
+};
---
+++ apps/nextra/pages/en/network/blockchain/data-structure-deep-dive/core-data-model/_meta.ts
@@ -0,0 +1,4 @@
+export default {
+  "state": "The State",
+  "ledger": "The Ledger",
+};
