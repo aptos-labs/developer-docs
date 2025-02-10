a383436966fca8697c15f460600cd4ff09de48ca
Rasa Welcher <kwelch12@gmail.com>
2025-02-10 13:12:58 -0500

    Renames Account Resource Permissions to Permissioned Signer

--- apps/nextra/pages/en/build/sdks/ts-sdk/account/_meta.tsx
+++ apps/nextra/pages/en/build/sdks/ts-sdk/account/_meta.tsx
@@ -2,7 +2,7 @@ export default {
   "account-abstraction": {
     title: "Account Abstraction",
   },
-  "account-resource-permissions": {
-    title: "Account Resource Permissions",
+  "permissioned-signer": {
+    title: "Permissioned Signer",
   },
 };
