1c5c553ba904e2d7de3e349c3b395498a0e7a712
Maayan <maayan@aptoslabs.com>
2025-03-12 17:35:14 -0400

    Refactor build e2e dapp guide (#851)
    * refactor build e2e dapp guide
    
    * address comments
    
    * handle redirects
    
    * add guide source code

--- apps/nextra/pages/en/build/guides/build-e2e-dapp/_meta.tsx
+++ apps/nextra/pages/en/build/guides/build-e2e-dapp/_meta.tsx
@@ -2,19 +2,16 @@ export default {
   "1-create-smart-contract": {
     title: "1. Create Smart Contract",
   },
-  "2-set-up-react-app": {
-    title: "2. Set Up React App",
+  "2-set-up-the-frontend": {
+    title: "2. Set Up The Frontend",
   },
-  "3-add-wallet-support": {
-    title: "3. Add Wallet Support",
+  "3-fetch-data-from-chain": {
+    title: "3. Fetch Data from Chain",
   },
-  "4-fetch-data-from-chain": {
-    title: "4. Fetch Data from Chain",
+  "4-submit-data-to-chain": {
+    title: "4. Submit Data to Chain",
   },
-  "5-submit-data-to-chain": {
-    title: "5. Submit Data to Chain",
-  },
-  "6-handle-tasks": {
-    title: "6. Handle Tasks",
+  "5-handle-tasks": {
+    title: "5. Handle Tasks",
   },
 };
