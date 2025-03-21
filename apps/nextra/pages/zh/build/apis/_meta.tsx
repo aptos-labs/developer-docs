9c97f77902bc159e1d51a45a4d4d2b4e55a074e4
Greg Nazario <greg@gnazar.io>
2025-03-21 09:44:20 -0400

    [chinese] Remove untranslated pages and redirect to english (#862)

--- apps/nextra/pages/zh/build/apis/_meta.tsx
+++ apps/nextra/pages/zh/build/apis/_meta.tsx
@@ -6,15 +6,23 @@ export default {
       layout: "full",
       sidebar: false,
     },
+    href: "/en/build/apis/fullnode-rest-api-reference",
   },
   "fullnode-rest-api": "全节点 REST API 指南",
   "indexer-graphql-api": {
     title: "索引器 GraphQL API",
     href: "/zh/build/indexer",
   },
-  "data-providers": "数据提供商",
+  "data-providers": {
+    title: "数据提供商",
+    href: "/en/build/apis/data-providers",
+  },
   "faucet-api": {
     title: "水龙头 API",
+    href: "/en/build/apis/faucet-api",
   },
-  "aptos-labs-developer-portal": "API 网关",
+  "aptos-labs-developer-portal": {
+    title: "API 网关",
+    href: "/en/build/apis/aptos-labs-developer-portal",
+  },
 };
