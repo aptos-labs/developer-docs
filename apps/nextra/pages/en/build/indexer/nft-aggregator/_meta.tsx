c9c8272de3d7a769c0c7fade6135473c852ef134
yuunlimm <dla0267@gmail.com>
2025-04-07 16:42:34 -0700

    add marketplaces page

--- apps/nextra/pages/en/build/indexer/nft-aggregator/_meta.tsx
+++ apps/nextra/pages/en/build/indexer/nft-aggregator/_meta.tsx
@@ -2,4 +2,13 @@ export default {
   "nft-aggregator-table": {
     title: "NFT Aggregator Table Reference",
   },
+  marketplaces: {
+    title: "Marketplaces",
+    items: [
+      {
+        title: "Bluemove",
+        href: "/en/build/indexer/nft-aggregator/marketplaces/bluemove",
+      },
+    ],
+  },
 };
